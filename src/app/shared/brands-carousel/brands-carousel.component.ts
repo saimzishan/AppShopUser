53eaaa07e71fadbb7729167fb2bbc896df011f5f
saimzishan@gmail.com <saimzishan@gmail.com>
2018-11-10 16:26:14 +0500

    reques in progress

--- src/app/shared/brands-carousel/brands-carousel.component.ts
+++ src/app/shared/brands-carousel/brands-carousel.component.ts
@@ -1,72 +1,68 @@
-import {AfterViewInit, Component, DoCheck, Input} from '@angular/core';
-import {SwiperConfigInterface} from 'ngx-swiper-wrapper';
-import {Router} from "@angular/router";
+import { AfterViewInit, Component, DoCheck, Input } from "@angular/core";
+import { SwiperConfigInterface } from "ngx-swiper-wrapper";
+import { Router } from "@angular/router";
 
 @Component({
-    selector: 'app-brands-carousel',
-    templateUrl: './brands-carousel.component.html',
-    styleUrls: ['./brands-carousel.component.scss']
+  selector: "app-brands-carousel",
+  templateUrl: "./brands-carousel.component.html",
+  styleUrls: ["./brands-carousel.component.scss"]
 })
 export class BrandsCarouselComponent implements DoCheck, AfterViewInit {
+  @Input("brands") brands: Array<any> = [];
 
-    @Input('brands') brands: Array<any> = [];
+  public config: SwiperConfigInterface = {};
 
-    public config: SwiperConfigInterface = {};
+  loadBrands = false;
 
-    loadBrands = false;
+  constructor(private router: Router) {}
 
-    constructor(private router: Router) {
+  ngDoCheck() {
+    if (this.brands.length) {
+      this.loadBrands = true;
     }
+  }
 
-    ngDoCheck() {
-        if (this.brands.length) {
-            this.loadBrands = true;
+  ngAfterViewInit() {
+    this.config = {
+      slidesPerView: 7,
+      spaceBetween: 16,
+      keyboard: true,
+      navigation: true,
+      pagination: false,
+      grabCursor: true,
+      loop: false,
+      preloadImages: false,
+      lazy: true,
+      autoplay: {
+        delay: 6000,
+        disableOnInteraction: false
+      },
+      speed: 500,
+      effect: "slide",
+      breakpoints: {
+        320: {
+          slidesPerView: 1
+        },
+        480: {
+          slidesPerView: 2
+        },
+        600: {
+          slidesPerView: 3
+        },
+        960: {
+          slidesPerView: 4
+        },
+        1280: {
+          slidesPerView: 5
+        },
+        1500: {
+          slidesPerView: 6
         }
-    }
+      }
+    };
+  }
 
-    ngAfterViewInit() {
-        this.config = {
-            slidesPerView: 7,
-            spaceBetween: 16,
-            keyboard: true,
-            navigation: true,
-            pagination: false,
-            grabCursor: true,
-            loop: false,
-            preloadImages: false,
-            lazy: true,
-            autoplay: {
-                delay: 6000,
-                disableOnInteraction: false
-            },
-            speed: 500,
-            effect: 'slide',
-            breakpoints: {
-                320: {
-                    slidesPerView: 1
-                },
-                480: {
-                    slidesPerView: 2
-                },
-                600: {
-                    slidesPerView: 3,
-                },
-                960: {
-                    slidesPerView: 4,
-                },
-                1280: {
-                    slidesPerView: 5,
-                },
-                1500: {
-                    slidesPerView: 6,
-                }
-            }
-        };
-    }
-
-    public getBrandProducts(brandId) {
-        console.log(brandId);
-        this.router.navigate(['/products/brand', brandId]);
-    }
-
+  public getBrandProducts(brandId) {
+    this.router.navigate(["/products/brand", brandId]);
+  }
 }
--- src/app/shared/main-carousel/main-carousel.component.ts
+++ src/app/shared/main-carousel/main-carousel.component.ts
@@ -1,62 +1,73 @@
-import {Component, OnInit, Input, AfterViewInit, DoCheck} from '@angular/core';
-import {SwiperConfigInterface, SwiperPaginationInterface} from 'ngx-swiper-wrapper';
-import {AppService} from "../../app.service";
-import {Router} from "@angular/router";
+import {
+  Component,
+  OnInit,
+  Input,
+  AfterViewInit,
+  DoCheck
+} from "@angular/core";
+import {
+  SwiperConfigInterface,
+  SwiperPaginationInterface
+} from "ngx-swiper-wrapper";
+import { AppService } from "../../app.service";
+import { Router } from "@angular/router";
 
 @Component({
-    selector: 'app-main-carousel',
-    templateUrl: './main-carousel.component.html',
-    styleUrls: ['./main-carousel.component.scss']
+  selector: "app-main-carousel",
+  templateUrl: "./main-carousel.component.html",
+  styleUrls: ["./main-carousel.component.scss"]
 })
 export class MainCarouselComponent implements OnInit, AfterViewInit, DoCheck {
-    @Input() slides: Array<any> = [];
+  @Input() slides: Array<any> = [];
 
-    public config: SwiperConfigInterface = {};
+  public config: SwiperConfigInterface = {};
 
-    private pagination: SwiperPaginationInterface = {
-        el: '.swiper-pagination',
-        clickable: true
+  private pagination: SwiperPaginationInterface = {
+    el: ".swiper-pagination",
+    clickable: true
+  };
+
+  loadSlider = false;
+
+  constructor(public appService: AppService, private router: Router) {}
+
+  ngOnInit() {
+    console.log(this.slides);
+  }
+
+  ngDoCheck() {
+    if (this.slides.length) {
+      this.loadSlider = true;
+    }
+  }
+
+  ngAfterViewInit() {
+    this.config = {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      keyboard: true,
+      navigation: true,
+      pagination: this.pagination,
+      grabCursor: true,
+      loop: false,
+      preloadImages: false,
+      lazy: true,
+      autoplay: {
+        delay: 6000,
+        disableOnInteraction: false
+      },
+      speed: 500,
+      effect: "slide"
     };
+  }
 
-    loadSlider = false;
-
-    constructor(public appService: AppService, private router: Router) {
-    }
-
-    ngOnInit() {
-        console.log(this.slides);
-    }
-
-    ngDoCheck() {
-        if (this.slides.length) {
-            this.loadSlider = true;
-        }
-    }
-
-    ngAfterViewInit() {
-        this.config = {
-            slidesPerView: 1,
-            spaceBetween: 0,
-            keyboard: true,
-            navigation: true,
-            pagination: this.pagination,
-            grabCursor: true,
-            loop: false,
-            preloadImages: false,
-            lazy: true,
-            autoplay: {
-                delay: 6000,
-                disableOnInteraction: false
-            },
-            speed: 500,
-            effect: 'slide'
-        };
-    }
-
-    public productDetail(product) {
-        console.log(product);
-        this.router.navigate(['/products', product.id, product.name, {supplier_id: product.suppliers[0].id}]);
-        // ['/products', product.id, product.name, {supplier_id: product.supplier_id}]
-
-    }
+  public productDetail(product) {
+    this.router.navigate([
+      "/products",
+      product.id,
+      product.name,
+      { supplier_id: product.suppliers[0].id }
+    ]);
+    // ['/products', product.id, product.name, {supplier_id: product.supplier_id}]
+  }
 }
